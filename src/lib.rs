d4bc9743aacf31b0717874a13c6aae280806a1bf
한진희 <0200134hjh@gmail.com>
2025-11-15 18:25:14 +0900

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,49 @@
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub enum IR {
+    // Literals
     Num(i64),
+    Str(String),
+
+    // Variables
+    Var(String),
+    Let { name: String, value: Box<IR> },
+
+    // Binary operations
     Add(Box<IR>, Box<IR>),
-    Mul(Box<IR>, Box<IR>),
     Sub(Box<IR>, Box<IR>),
+    Mul(Box<IR>, Box<IR>),
     Div(Box<IR>, Box<IR>),
+
+    // Logic
+    Eq(Box<IR>, Box<IR>),
+    Lt(Box<IR>, Box<IR>),
+    Gt(Box<IR>, Box<IR>),
+
+    // Blocks / Scope
     Block(Vec<IR>),
+
+    // Control flow
+    If {
+        cond: Box<IR>,
+        then_branch: Box<IR>,
+        else_branch: Box<IR>,
+    },
+
+    // Function call
+    Call {
+        name: String,
+        args: Vec<IR>,
+    },
+
+    // Function definition
+    FuncDef {
+        name: String,
+        params: Vec<String>,
+        body: Box<IR>,
+    },
+
+    // Return
+    Return(Box<IR>),
 }
